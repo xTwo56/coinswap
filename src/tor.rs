06bf0563850a99164e5f46bb90847c59bb976e70
Shourya Sharma <sshourya17@gmail.com>
2024-01-03 11:49:16 -0500

    Added test to check if tor is running or not

--- src/tor.rs
+++ src/tor.rs
@@ -4,19 +4,59 @@ const SOCKS_PORT: u16 = 19_050;
 const CHECK_URL: &str = "https://check.torproject.org";
 
 fn tor_instance() {
-    match Tor::new().flag(TorFlag::DataDirectory("/temp/tor-rust".into())).flag(TorFlag::SocksPort(SOCKS_PORT)).flag(TorFlag::HiddenServiceDir("/tmp/tor-rust/hs-dir".into())).flag(TorFlag::HiddenServiceVersion(HiddenServiceVersion::V3)).flag(TorFlag::HiddenServicePort(TorAddress::Port(8000), None.into())).start() {
-        Ok(r)=> println!("Here success: {:?}",r),
-        Err(e) => println!("Here Error:{:?}",e)
+    match Tor::new()
+        .flag(TorFlag::DataDirectory("/tmp/tor-rust".into()))
+        .flag(TorFlag::SocksPort(SOCKS_PORT))
+        .flag(TorFlag::HiddenServiceDir("/tmp/tor-rust/hs-dir".into()))
+        .flag(TorFlag::HiddenServiceVersion(HiddenServiceVersion::V3))
+        .flag(TorFlag::HiddenServicePort(
+            TorAddress::Port(8000),
+            None.into(),
+        ))
+        .start()
+    {
+        Ok(r) => println!("tor exit result: {}", r),
+        Err(e) => eprintln!("tor error: {}", e),
     };
 }
 
 #[cfg(test)]
 mod test {
+    use std::{thread, time::Duration};
 
+    use tokio::task;
     use crate::tor::*;
 
-    #[test]
-    fn test_instance() {
-        tor_instance();
+    #[tokio::test]
+    async fn test_instance() {
+        task::spawn_blocking(|| tor_instance());
+        
+        let proxy = reqwest::Proxy::all(format!("socks5://127.0.0.1:{}", SOCKS_PORT)).expect("Where is proxy");
+        let client = reqwest::Client::builder().proxy(proxy).build().expect("Client should we build");
+
+        thread::sleep(Duration::from_millis(2500));
+        println!("---------------------------------request--------------------------------------------");
+        let response = client
+        .get(CHECK_URL.to_string())
+        .send()
+        .await;
+        match response {
+            Ok(r) => {
+                let html = r.text().await.unwrap();
+                println!("{:?}",html);
+                match html.find("Congratulations") {
+                    Some(_) => {
+                        println!("Tor is online!");
+                    }
+                    None => {
+                        println!("Received a response but not through Tor...");
+                    }
+                }
+            }
+            Err(e) => {
+                println!("error: {}", e.to_string());
+            }
+        };
+    
     }
 }
