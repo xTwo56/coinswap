6067d1e9c9b76f77ff3b9c86cda328c1a034828c
Shourya Sharma <sshourya17@gmail.com>
2024-03-26 22:22:21 -0400

    Added defualt directory config and address file

--- src/market/directory.rs
+++ src/market/directory.rs
@@ -2,15 +2,10 @@
 //!
 //! Handles market-related logic where Makers post their offers. Also provides functions to synchronize
 //! maker addresses from directory servers, post maker addresses to directory servers,
-//! and defines constants such as Tor addresses and directory server addresses.
-
-/// Represents the Tor address and port configuration.
-// It should be set to your specific Tor address and port.
-pub const TOR_SOCKS_ADDR: &str = "127.0.0.1:19050";
 
 use std::{
     collections::HashSet,
-    fs,
+    fs, io,
     sync::{Arc, RwLock},
     thread,
     time::Duration,
@@ -18,15 +13,16 @@ use std::{
 
 use std::path::{Path, PathBuf};
 
-use crate::utill::{kill_tor_handles, monitor_log_for_completion, spawn_tor};
+use crate::utill::{
+    get_data_dir, kill_tor_handles, monitor_log_for_completion, parse_field, parse_toml, spawn_tor,
+    write_default_config,
+};
 
 use tokio::{
     fs::OpenOptions,
     io::{AsyncBufReadExt, AsyncWriteExt},
     net::TcpListener,
 };
-
-const ADDRESS_FILE: &str = "/tmp/maker_addresses.dat";
 
 /// Represents errors that can occur during directory server operations.
 #[derive(Debug)]
@@ -35,17 +31,69 @@ pub enum DirectoryServerError {
     Other(&'static str),
 }
 
+/// Directory Configuration,
+#[derive(Debug)]
 pub struct DirectoryServer {
     pub port: u16,
     pub socks_port: u16,
     pub shutdown: RwLock<bool>,
 }
 
+impl Default for DirectoryServer {
+    fn default() -> Self {
+        Self {
+            port: 8080,
+            socks_port: 19060,
+            shutdown: RwLock::new(false),
+        }
+    }
+}
+
 impl DirectoryServer {
-    pub fn init(port: Option<u16>, socks_port: Option<u16>) -> Result<Self, DirectoryServerError> {
-        Ok(Self {
-            port: port.unwrap_or(8080),
-            socks_port: socks_port.unwrap_or(19060),
+    /// Constructs a [DirectoryConfig] from a specified data directory. Or create default configs and load them.
+    ///
+    /// The directory.toml file should exist at the provided data-dir location.
+    /// Or else, a new default-config will be loaded and created at given data-dir location.
+    /// If no data-dir is provided, a default config will be created at default data-dir location.
+    ///
+    /// For reference of default config checkout `./directory.toml` in repo folder.
+    ///
+    /// Default data-dir for linux: `~/.coinswap/`
+    /// Default config locations: `~/.coinswap/directory_server/configs/directory.toml`.
+
+    pub fn new(config_path: Option<&PathBuf>) -> io::Result<Self> {
+        let default_config = Self::default();
+
+        let default_config_path = get_data_dir()
+            .join("directory_server")
+            .join("config")
+            .join("directory.toml");
+        let config_path = config_path.unwrap_or(&default_config_path);
+
+        if !config_path.exists() {
+            write_default_directory_config(config_path);
+            log::warn!(
+                "Directory config file not found, creating default config file at path: {}",
+                config_path.display()
+            );
+        }
+
+        let section = parse_toml(config_path)?;
+        log::info!(
+            "Successfully loaded config file from : {}",
+            config_path.display()
+        );
+
+        let directory_config_section = section.get("maker_config").cloned().unwrap_or_default();
+
+        Ok(DirectoryServer {
+            port: parse_field(directory_config_section.get("port"), default_config.port)
+                .unwrap_or(default_config.port),
+            socks_port: parse_field(
+                directory_config_section.get("socks_port"),
+                default_config.socks_port,
+            )
+            .unwrap_or(default_config.socks_port),
             shutdown: RwLock::new(false),
         })
     }
@@ -60,6 +108,18 @@ impl DirectoryServer {
     }
 }
 
+fn write_default_directory_config(config_path: &PathBuf) {
+    let config_string = String::from(
+        "\
+            [directory_config]\n\
+            port = 8080\n\
+            socks_port = 19060\n\
+            ",
+    );
+
+    write_default_config(config_path, config_string).unwrap();
+}
+
 impl From<reqwest::Error> for DirectoryServerError {
     fn from(e: reqwest::Error) -> DirectoryServerError {
         DirectoryServerError::Reqwest(e)
@@ -69,6 +129,8 @@ impl From<reqwest::Error> for DirectoryServerError {
 #[tokio::main]
 pub async fn start_directory_server(directory: Arc<DirectoryServer>) {
     log::info!("Inside Directory Server");
+
+    let address_file = get_data_dir().join("directory_server").join("address.dat");
 
     let tor_log_dir = "/tmp/tor-rust-directory/log".to_string();
     if Path::new(tor_log_dir.as_str()).exists() {
@@ -113,7 +175,7 @@ pub async fn start_directory_server(directory: Arc<DirectoryServer>) {
                     .write(true)
                     .create(true)
                     .truncate(true)
-                    .open(ADDRESS_FILE)
+                    .open(address_file.to_str().unwrap())
                     .await
                     .unwrap();
                     for addr in &addresses {
@@ -141,5 +203,79 @@ async fn handle_client(mut stream: tokio::net::TcpStream, addresses: &mut HashSe
             .iter()
             .fold(String::new(), |acc, addr| acc + addr + "\n");
         stream.write_all(response.as_bytes()).await.unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utill::get_home_dir;
+
+    use super::*;
+    use std::{fs::File, io::Write};
+
+    fn create_temp_config(contents: &str, file_name: &str) -> PathBuf {
+        let file_path = PathBuf::from(file_name);
+        let mut file = File::create(&file_path).unwrap();
+        writeln!(file, "{}", contents).unwrap();
+        file_path
+    }
+
+    fn remove_temp_config(path: &PathBuf) {
+        fs::remove_file(path).unwrap();
+    }
+
+    #[test]
+    fn test_valid_config() {
+        let contents = r#"
+            [directory_config]
+            port = 8080
+            socks_port = 19060
+        "#;
+        let config_path = create_temp_config(contents, "valid_directory_config.toml");
+        let config = DirectoryServer::new(Some(&config_path)).unwrap();
+        remove_temp_config(&config_path);
+
+        let default_config = DirectoryServer::default();
+        assert_eq!(config.port, default_config.port);
+        assert_eq!(config.socks_port, default_config.socks_port);
+    }
+
+    #[test]
+    fn test_missing_fields() {
+        let contents = r#"
+            [directory_config]
+            port = 8080
+        "#;
+        let config_path = create_temp_config(contents, "missing_fields_directory_config.toml");
+        let config = DirectoryServer::new(Some(&config_path)).unwrap();
+        remove_temp_config(&config_path);
+
+        assert_eq!(config.port, 8080);
+        assert_eq!(config.socks_port, DirectoryServer::default().socks_port);
+    }
+
+    #[test]
+    fn test_incorrect_data_type() {
+        let contents = r#"
+            [directory_config]
+            port = "not_a_number"
+        "#;
+        let config_path = create_temp_config(contents, "incorrect_type_directory_config.toml");
+        let config = DirectoryServer::new(Some(&config_path)).unwrap();
+        remove_temp_config(&config_path);
+
+        let default_config = DirectoryServer::default();
+        assert_eq!(config.port, default_config.port);
+        assert_eq!(config.socks_port, default_config.socks_port);
+    }
+
+    #[test]
+    fn test_missing_file() {
+        let config_path = get_home_dir().join("directory.toml");
+        let config = DirectoryServer::new(Some(&config_path)).unwrap();
+        remove_temp_config(&config_path);
+        let default_config = DirectoryServer::default();
+        assert_eq!(config.port, default_config.port);
+        assert_eq!(config.socks_port, default_config.socks_port);
     }
 }
--- tests/malice2.rs
+++ tests/malice2.rs
@@ -35,8 +35,7 @@ async fn malice2_maker_broadcast_contract_prematurely() {
 
     info!("Initiating Directory Server .....");
 
-    let directory_server_instance =
-        Arc::new(DirectoryServer::init(Some(8080), Some(19060)).unwrap());
+    let directory_server_instance = Arc::new(DirectoryServer::new(None).unwrap());
     let directory_server_instance_clone = directory_server_instance.clone();
     thread::spawn(move || {
         start_directory_server(directory_server_instance_clone);
