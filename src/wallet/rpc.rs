f737d3556b4b417e971fc287082101714fd571ec
Shourya Sharma <sshourya17@gmail.com>
2024-04-23 23:27:47 -0400

    isolated the collect descriptor to api

--- src/wallet/rpc.rs
+++ src/wallet/rpc.rs
@@ -1,19 +1,16 @@
 //! Manages connection with a Bitcoin Core RPC.
 //!
-use std::{ convert::TryFrom, thread, time::Duration };
+use std::{convert::TryFrom, thread, time::Duration};
 
 use bitcoin::Network;
-use bitcoind::bitcoincore_rpc::{ Auth, Client, RpcApi };
+use bitcoind::bitcoincore_rpc::{Auth, Client, RpcApi};
 use serde_json::Value;
 
-use crate::{
-    utill::{ redeemscript_to_scriptpubkey, str_to_bitcoin_network },
-    wallet::{ api::KeychainKind, WalletSwapCoin },
-};
+use crate::{utill::str_to_bitcoin_network, wallet::api::KeychainKind};
 
 use serde::Deserialize;
 
-use super::{ error::WalletError, Wallet };
+use super::{error::WalletError, Wallet};
 
 /// Configuration parameters for connecting to a Bitcoin node via RPC.
 #[derive(Debug, Clone)]
@@ -49,13 +46,14 @@ impl TryFrom<&RPCConfig> for Client {
                 "http://{}/wallet/{}",
                 config.url.as_str(),
                 config.wallet_name.as_str()
-            ).as_str(),
-            config.auth.clone()
+            )
+            .as_str(),
+            config.auth.clone(),
         )?;
         if config.network != str_to_bitcoin_network(rpc.get_blockchain_info()?.chain.as_str()) {
-            return Err(
-                WalletError::Protocol("RPC Network not mathcing with RPCConfig".to_string())
-            );
+            return Err(WalletError::Protocol(
+                "RPC Network not mathcing with RPCConfig".to_string(),
+            ));
         }
         Ok(rpc)
     }
@@ -72,12 +70,7 @@ fn list_wallet_dir(client: &Client) -> Result<Vec<String>, WalletError> {
     }
 
     let result: CallResult = client.call("listwalletdir", &[])?;
-    Ok(
-        result.wallets
-            .into_iter()
-            .map(|n| n.name)
-            .collect()
-    )
+    Ok(result.wallets.into_iter().map(|n| n.name).collect())
 }
 
 impl Wallet {
@@ -93,16 +86,17 @@ impl Wallet {
         } else {
             // pre-0.21 use legacy wallets
             if self.rpc.version()? < 210_000 {
-                self.rpc.create_wallet(wallet_name, Some(true), None, None, None)?;
+                self.rpc
+                    .create_wallet(wallet_name, Some(true), None, None, None)?;
             } else {
                 // TODO: move back to api call when https://github.com/rust-bitcoin/rust-bitcoincore-rpc/issues/225 is closed
                 let args = [
                     Value::String(wallet_name.clone()),
-                    Value::Bool(true), // Disable Private Keys
+                    Value::Bool(true),  // Disable Private Keys
                     Value::Bool(false), // Create a blank wallet
-                    Value::Null, // Optional Passphrase
+                    Value::Null,        // Optional Passphrase
                     Value::Bool(false), // Avoid Reuse
-                    Value::Bool(true), // Descriptor Wallet
+                    Value::Bool(true),  // Descriptor Wallet
                 ];
                 let _: Value = self.rpc.call("createwallet", &args)?;
             }
@@ -110,95 +104,52 @@ impl Wallet {
             log::info!("wallet created: {}", wallet_name);
         }
 
-        let mut descriptors_to_import = Vec::new();
-
-        descriptors_to_import.extend(self.get_unimported_wallet_desc()?);
-
-        descriptors_to_import.extend(
-            self.store.incoming_swapcoins
-                .values()
-                .map(|sc| {
-                    format!("wsh(sortedmulti(2,{},{}))", sc.get_other_pubkey(), sc.get_my_pubkey())
-                })
-                .map(|d| self.rpc.get_descriptor_info(&d).unwrap().descriptor)
-                .filter(|d| !self.is_descriptor_imported(d))
-                .collect::<Vec<String>>()
-        );
-
-        descriptors_to_import.extend(
-            self.store.outgoing_swapcoins
-                .values()
-                .map(|sc| {
-                    format!("wsh(sortedmulti(2,{},{}))", sc.get_other_pubkey(), sc.get_my_pubkey())
-                })
-                .map(|d| self.rpc.get_descriptor_info(&d).unwrap().descriptor)
-                .filter(|d| !self.is_descriptor_imported(d))
-        );
-
-        descriptors_to_import.extend(
-            self.store.incoming_swapcoins
-                .values()
-                .map(|sc| {
-                    let contract_spk = redeemscript_to_scriptpubkey(&sc.contract_redeemscript);
-                    format!("raw({:x})", contract_spk)
-                })
-                .map(|d| self.rpc.get_descriptor_info(&d).unwrap().descriptor)
-                .filter(|d| !self.is_descriptor_imported(d))
-                .collect::<Vec<_>>()
-        );
-        descriptors_to_import.extend(
-            self.store.outgoing_swapcoins
-                .values()
-                .map(|sc| {
-                    let contract_spk = redeemscript_to_scriptpubkey(&sc.contract_redeemscript);
-                    format!("raw({:x})", contract_spk)
-                })
-                .map(|d| self.rpc.get_descriptor_info(&d).unwrap().descriptor)
-                .filter(|d| !self.is_descriptor_imported(d))
-                .collect::<Vec<_>>()
-        );
+        let descriptors_to_import = self.collect_descriptors_to_import()?;
 
         let is_fidelity_addrs_imported = {
-            let mut spks = self.store.fidelity_bond.iter().map(|(_, (b, _, _))| b.script_pub_key());
+            let mut spks = self
+                .store
+                .fidelity_bond
+                .iter()
+                .map(|(_, (b, _, _))| b.script_pub_key());
             let (first_addr, last_addr) = (spks.next(), spks.last());
 
             let is_first_imported = if let Some(spk) = first_addr {
                 let descriptor_without_checksum = format!("raw({:x})", spk);
-                let descriptor = self.rpc
+                let descriptor = self
+                    .rpc
                     .get_descriptor_info(&descriptor_without_checksum)
-                    .unwrap().descriptor;
+                    .unwrap()
+                    .descriptor;
                 let addr = self.rpc.derive_addresses(&descriptor, None).unwrap()[0].clone();
                 self.rpc
                     .get_address_info(&addr.assume_checked())
                     .unwrap()
-                    .is_watchonly.unwrap_or(false)
+                    .is_watchonly
+                    .unwrap_or(false)
             } else {
                 true // mark true if theres no spk to import
             };
 
             let is_last_imported = if let Some(spk) = last_addr {
                 let descriptor_without_checksum = format!("raw({:x})", spk);
-                let descriptor = self.rpc
+                let descriptor = self
+                    .rpc
                     .get_descriptor_info(&descriptor_without_checksum)
-                    .unwrap().descriptor;
+                    .unwrap()
+                    .descriptor;
                 let addr = self.rpc.derive_addresses(&descriptor, None).unwrap()[0].clone();
                 self.rpc
                     .get_address_info(&addr.assume_checked())
                     .unwrap()
-                    .is_watchonly.unwrap_or(false)
+                    .is_watchonly
+                    .unwrap_or(false)
             } else {
                 true // mark true if theres no spks to import
             };
 
             is_first_imported && is_last_imported
         };
-
-        descriptors_to_import.extend(
-            self.store.fidelity_bond.iter().map(|(_, (_, spk, _))| {
-                let descriptor_without_checksum = format!("raw({:x})", spk);
-                self.rpc.get_descriptor_info(&descriptor_without_checksum).unwrap().descriptor
-            })
-        );
 
         if descriptors_to_import.is_empty() && is_fidelity_addrs_imported {
             return Ok(());
@@ -214,17 +165,21 @@ impl Wallet {
         // Sometimes in test multiple wallet scans can occur at same time, resulting in error.
         // Just retry after 3 sec.
         loop {
-            let last_synced_height = self.store.last_synced_height
+            let last_synced_height = self
+                .store
+                .last_synced_height
                 .unwrap_or(0)
                 .max(self.store.wallet_birthday.unwrap_or(0));
             let node_synced = self.rpc.get_block_count()?;
-            log::info!("rescan_blockchain from:{} to:{}", last_synced_height, node_synced);
-            match
-                self.rpc.rescan_blockchain(
-                    Some(last_synced_height as usize),
-                    Some(node_synced as usize)
-                )
-            {
+            log::info!(
+                "rescan_blockchain from:{} to:{}",
+                last_synced_height,
+                node_synced
+            );
+            match self.rpc.rescan_blockchain(
+                Some(last_synced_height as usize),
+                Some(node_synced as usize),
+            ) {
                 Ok(_) => {
                     self.store.last_synced_height = Some(node_synced);
                     break;
