5dea2dfb44f8935b65fb431f0979cd0efdf0c124
mojoX911 <mojojojo48@protonmail.com>
2025-01-04 16:15:33 +0530

    test fix

--- src/maker/config.rs
+++ src/maker/config.rs
@@ -177,7 +177,7 @@ mod tests {
             network_port = 6102
             rpc_port = 6103
             required_confirms = 1
-            min_swap_amount = 100000
+            min_swap_amount = 10000
             socks_port = 19050
         "#;
         let config_path = create_temp_config(contents, "valid_maker_config.toml");
